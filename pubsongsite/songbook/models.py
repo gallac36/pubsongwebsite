7de101ef92223134e67ccd5c15315e33d25a4602
ConorG <conor.gallagher2@mail.dcu.ie>
2018-05-11 17:59:14 +0100

    First commit

---
+++ pubsongsite/songbook/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    lyrics = models.TextField()
+    author = models.CharField(max_length=50)
+    chords = models.TextField(blank=True)
+
+
+def __str__(self):
+    return self.field_name
